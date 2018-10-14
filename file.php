840c0104b9a14dba4e05930dc084d52edf1e0679
wuying <wuy_123@126.com>
2018-10-14 15:07:38 +0800

    fellows25_php

---
+++ file.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+
+<form action="upload_file.php" method="post"
+      enctype="multipart/form-data">
+    Filename: <input type="file" name="file" id="file" />
+    <br />
+    <input type="submit" name="submit" value="Submit" />
+</form>
+
+</body>
+</html>
