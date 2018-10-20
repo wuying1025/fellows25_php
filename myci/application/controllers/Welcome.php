b4f3a7523473949bb55572d3b01201b064c40ef0
wuying <wuy_123@126.com>
2018-10-20 11:23:55 +0800

    ci

--- myci/application/controllers/Welcome.php
+++ myci/application/controllers/Welcome.php
@@ -28,6 +28,38 @@ class Welcome extends CI_Controller {
 
 	}
 
+	public function regist(){
+		$this->load->view('regist');
+
+	}
+
+	public function save(){
+
+		//1.接收数据
+
+		$name = $this->input->post('username');
+		$pwd1 = $this->input->post('pwd1');
+		$pwd2 = $this->input->post('pwd2');
+
+		$data = array(
+			'name_error' => '用户名不能为空'
+		);
+		//2.验证
+
+		if($name == ''){
+			$this -> load->view('regist',$data);
+		}
+
+		//3.连接数据库
+
+
+		//4.跳转页面
+
+
+
+//		echo 'save...';
+	}
+
 //   http://localhost/myci/welcome/login
 
 //   http://localhost/项目名/控制器名/方法名
--- myci/application/views/regist.php
+++ myci/application/views/regist.php
@@ -4,11 +4,17 @@
     <meta charset="UTF-8">
     <title>注册</title>
     <base href="<?php echo site_url();?>">
+    <style>
+        .error{
+            color:red;
+        }
+    </style>
 </head>
 <body>
 <form action="welcome/save" method="post">
     <p>
         用户名: <input type="text" name="username">
+        <span class="error"><?php echo isset($name_error)? $name_error:''?></span>
     </p>
     <p>
         密码: <input type="password" name="pwd1">
